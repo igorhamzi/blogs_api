92a97de98752ad8d0a99d909ac1acaf8616f3835
IgorHamzi <igorwhamzi@gmail.com>
2022-05-08 20:01:04 -0300

    adiciona requisito 8 e 9

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const userRoutes = require('./src/routes/userRoutes');
 const loginRoutes = require('./src/routes/loginRoutes');
 const categoriesRoutes = require('./src/routes/categoriesRoutes');
+const blogPostsRoutes = require('./src/routes/blogPostsRoutes');
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use('/user', userRoutes);
 app.use('/login', loginRoutes);
 app.use('/categories', categoriesRoutes);
+app.use('/post', blogPostsRoutes);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
